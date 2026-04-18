c02ff637a89efd0bb45bc6bbf8086f43f826557b
SantiagoQP31 <santiagoqp3103@gmail.com>
2026-04-18 12:02:58 -0500

    feat(network): agregar outputs raiz del proyecto

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,24 @@
-# Los outputs se agregarán cuando los módulos estén listos
+output "vpc_id" {
+  description = "ID de la VPC"
+  value       = module.network.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs de las subnets publicas"
+  value       = module.network.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs de las subnets privadas"
+  value       = module.network.private_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "ID del Internet Gateway"
+  value       = module.network.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  description = "ID del NAT Gateway"
+  value       = module.network.nat_gateway_id
+}
